9c8cf502a1e85c0d1978602dd1f0dda954f0eac3
Zolzaya99 <zolzayaoyunkhand@gmail.com>
2023-09-19 18:38:09 -0600

    removed actual url

--- demo.js
+++ demo.js
@@ -1,7 +1,6 @@
 const {MongoClient} = require('mongodb')
 
 async function main() {
-    // const url = "mongodb+srv://oyu20001:Zoloo0527@cluster0.zpweeeh.mongodb.net/?retryWrites=true&w=majority";
 
     const client= new MongoClient(url);
 
