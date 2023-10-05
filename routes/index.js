8f30f9617cf84c820b01ce2734ae3b1eb6d05ef6
Zolzaya99 <zolzayaoyunkhand@gmail.com>
2023-10-04 21:43:20 -0600

    swagger change

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const routes = require('express').Router();
 
-const baseController = require('../controllers/contacts');
+// const baseController = require('../controllers/contacts');
 
-// routes.get('/', baseController.getName);
-// routes.get('/test', baseController.getNameTwo);
+routes.use('/', require('./swagger'))
 routes.use('/contacts', require('./contacts'));
 
 module.exports = routes;
