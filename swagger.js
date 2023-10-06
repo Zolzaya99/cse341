6ac67548fcd0d9de2664703fb2f2d6bd56ff3d1c
Zolzaya99 <zolzayaoyunkhand@gmail.com>
2023-10-06 11:01:17 -0600

    created new swagger json to test.

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'My Test API attempt',
   },
-  host: 'https://week2-personal-assignment.onrender.com/',
+  host: 'https://week2-personal-assignment.onrender.com',
   schemes: ['https'],
 };
 
